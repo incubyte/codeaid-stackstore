80a9cde16a68c173993de99e03c8bd979485144a
Karen Abramowitz <karenabramowitz@gmail.com>
2016-06-07 18:07:40 -0400

    cleaned up and added individual and category dream routes

--- server/app/routes/dreams.js
+++ server/app/routes/dreams.js
@@ -8,9 +8,34 @@ module.exports = router;
 
 router.get('/', function(req, res, next) {
     Dream.findAll({ where: req.query })
-        .then(function(dreams) { 
-        	res.json(dreams);
+        .then(function(dreams) {
+            res.json(dreams);
         })
         .catch(next);
 });
 
+router.get('/:id', function(req, res, next) {
+    Dream.findOne({
+            where: { id: req.params.id }
+        })
+        .then(function(theDream) {
+            res.json(theDream);
+        })
+        .catch(next);
+});
+
+router.get('/category/:category', function(req, res, next) {
+    Dream.findAll({
+            where: {
+                category: {
+                    $contains: [req.params.category]
+                }
+            }
+
+
+        })
+        .then(function(dreams) {
+            res.json(dreams);
+        })
+        .catch(next);
+});
--- server/db/index.js
+++ server/db/index.js
@@ -12,31 +12,6 @@ Cart.belongsTo(User);
 Review.belongsTo(User);
 Order.belongsTo(User);
 
-console.log("What's the dealio?", Cart.associations);
+// console.log("What's the dealio?", Cart.associations);
 
 
-Dream.create({
-        title: "Classic",
-        description: "hello",
-        price: 12.33,
-        quantity: 12,
-        category: ["hi"],
-        photo: "I'm a photo"
-    })
-    // 'use strict';
-    // const db = require('./db');
-    // const chalk = require('chalk');
-
-// // Require our models. Running each module registers the model into sequelize
-// // so any other part of the application can simply call sequelize.model('User')
-// // to get access to the User model.
-// require('./models');
-
-// // Syncing all the models at once. This promise is used by main.js.
-// var syncedDbPromise = db.sync();
-
-// syncedDbPromise.then(function () {
-//   console.log(chalk.green('Sequelize models synced to PostgreSQL'));
-// });
-
-// module.exports = syncedDbPromise;
