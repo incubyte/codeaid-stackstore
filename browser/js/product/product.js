4c62d01b5b3cbd614c253b2e950d960eb48ebcf5
Danielle Sheehan <danibsheehan@gmail.com>
2016-06-08 18:31:21 -0400

    product detail page in progress

---
+++ browser/js/product/product.js
@@ -0,0 +1,27 @@
+app.config(function($stateProvider){
+	$stateProvider.state('product', {
+		url: '/api/dreams/:id',
+		controller: 'ProductCtrl',
+		templateUrl: 'js/product/product.html',
+		resolve: {
+			productListing: function(ProductFactory, $stateParams){
+				return ProductFactory.getDream($stateParams.id);
+			}
+		}
+	});
+});
+
+app.factory('ProductFactory', function($http){
+	var ProductFactory = {};
+	ProductFactory.getDream = function(id){
+		return $http.get('/api/dreams/' + id)
+		.then(function(dream){
+			return dream.data;
+		})
+	}
+	return ProductFactory;
+});
+
+app.controller('ProductCtrl', function($scope, productListing){
+	$scope.product = productListing;
+});
