e077da8ad79fc78532b6be3143965a84e6496703
Danielle Sheehan <danibsheehan@gmail.com>
2016-06-08 17:12:12 -0400

    functioning homepage and category pages

--- browser/js/docs/docs.js
+++ browser/js/docs/docs.js
@@ -1,6 +1,39 @@
-app.config(function ($stateProvider) {
-    $stateProvider.state('docs', {
-        url: '/docs',
-        templateUrl: 'js/docs/docs.html'
-    });
+// app.config(function ($stateProvider) {
+//     $stateProvider.state('docs', {
+//         url: '/docs',
+//         templateUrl: 'js/docs/docs.html'
+//     });
+// });
+
+app.config(function($stateProvider){
+    $stateProvider.state('Cat', {
+        url: '/:category',
+        controller: 'CategoryCtrl',
+        templateUrl: 'js/docs/docs.html',
+        resolve: {
+        	theDreams: function(CategoryFactory, $stateParams){
+        		console.log($stateParams.category);
+        		return CategoryFactory.findOne($stateParams.category);
+        	}
+        }
+    })
 });
+
+
+app.factory('CategoryFactory', function($http, DreamsFactory){
+	var CategoryFactory = {};
+	CategoryFactory.findOne = function(category){
+		return $http.get('api/dreams/category/' + category)
+		.then(function(response){
+			return response.data;
+		});
+	};
+	return CategoryFactory;
+});
+
+app.controller('CategoryCtrl', function($scope, theDreams){
+	$scope.dreams = theDreams;
+});
+
+
+
