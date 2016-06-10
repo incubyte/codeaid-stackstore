8038aef827fcd70ca5e07bee9aab6844a4a54df7
ohhheyyyy <ashley.hartt@gmail.com>
2016-06-09 23:55:40 -0400

    Write additional users routes, update formatting, reinsert classes on product elements

--- browser/js/dreams/dream-list.directive.js
+++ browser/js/dreams/dream-list.directive.js
@@ -1,11 +1,11 @@
 'use strict';
 
-app.directive('dreamList', function () {
-  return {
-    restrict: 'E',
-    templateUrl: '/js/dreams/templates/dream-list.html',
-    scope: {
-      theDreams: '='
-    }
-  };
+app.directive('dreamList', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/js/dreams/templates/dream-list.html',
+        scope: {
+            theDreams: '='
+        }
+    };
 });
--- browser/js/dreams/dream-product.js
+++ browser/js/dreams/dream-product.js
@@ -1,27 +1,27 @@
-app.config(function($stateProvider){
-  $stateProvider.state('product', {
-    url: '/api/dreams/:id',
-    controller: 'ProductCtrl',
-    templateUrl: 'js/dreams/templates/dream-product.html',
-    resolve: {
-      productListing: function(ProductFactory, $stateParams){
-        return ProductFactory.getDream($stateParams.id);
-      }
-    }
-  });
+app.config(function($stateProvider) {
+    $stateProvider.state('product', {
+        url: '/api/dreams/:id',
+        controller: 'ProductCtrl',
+        templateUrl: 'js/dreams/templates/dream-product.html',
+        resolve: {
+            productListing: function(ProductFactory, $stateParams) {
+                return ProductFactory.getDream($stateParams.id);
+            }
+        }
+    });
 });
 
-app.factory('ProductFactory', function($http){
-  var ProductFactory = {};
-  ProductFactory.getDream = function(id){
-    return $http.get('/api/dreams/' + id)
-    .then(function(dream){
-      return dream.data;
-    })
-  }
-  return ProductFactory;
+app.factory('ProductFactory', function($http) {
+    var ProductFactory = {};
+    ProductFactory.getDream = function(id) {
+        return $http.get('/api/dreams/' + id)
+            .then(function(dream) {
+                return dream.data;
+            });
+    };
+    return ProductFactory;
 });
 
-app.controller('ProductCtrl', function($scope, productListing){
-  $scope.product = productListing;
+app.controller('ProductCtrl', function($scope, productListing) {
+    $scope.product = productListing;
 });
