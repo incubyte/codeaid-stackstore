7daebda61e34af3ac5f8cac171e93dbf9ddb2ab7
yogendra jaiswal <yogendra@incubyte.co>
2023-12-13 12:16:26 +0530

    tests passing

--- server/env/development.js
+++ server/env/development.js
@@ -1,19 +1,20 @@
 module.exports = {
-  "DATABASE_URI": "postgres://localhost:5432/fsg",
-  "SESSION_SECRET": "Optimus Prime is my real dad",
-  "TWITTER": {
-    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
-    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
-    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
+  DATABASE_URI: "postgres://postgres:pass@localhost:5432/fsg",
+  SESSION_SECRET: "Optimus Prime is my real dad",
+  TWITTER: {
+    consumerKey: "INSERT_TWITTER_CONSUMER_KEY_HERE",
+    consumerSecret: "INSERT_TWITTER_CONSUMER_SECRET_HERE",
+    callbackUrl: "INSERT_TWITTER_CALLBACK_HERE",
   },
-  "FACEBOOK": {
-    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
-    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
+  FACEBOOK: {
+    clientID: "INSERT_FACEBOOK_CLIENTID_HERE",
+    clientSecret: "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
+    callbackURL: "INSERT_FACEBOOK_CALLBACK_HERE",
   },
-  "GOOGLE": {
-    "clientID": "835524316110-vomeom3l2j1nl51mmtat36eqh9q3kqre.apps.googleusercontent.com",
-    "clientSecret": "Wf5uUMSExMklPWzeOyzM-BRd",
-    "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
-  }
+  GOOGLE: {
+    clientID:
+      "835524316110-vomeom3l2j1nl51mmtat36eqh9q3kqre.apps.googleusercontent.com",
+    clientSecret: "Wf5uUMSExMklPWzeOyzM-BRd",
+    callbackURL: "http://127.0.0.1:1337/auth/google/callback",
+  },
 };
--- tests/server/routes/cart-test.js
+++ tests/server/routes/cart-test.js
@@ -1,72 +1,76 @@
 // Instantiate all models
 
-process.env.NODE_ENV = 'testing';
+process.env.NODE_ENV = "testing";
 //We are changing the environment to testing in order to switch to the testing database (see server/env/index.js)
 
-var expect = require('chai').expect;
-var should = require('chai').should;
-var Sequelize = require('sequelize');
-var supertest = require('supertest');
+var expect = require("chai").expect;
+var should = require("chai").should;
+var Sequelize = require("sequelize");
+var supertest = require("supertest");
 
-var db = require('../../../server/db');
+var db = require("../../../server/db");
 
-describe('Cart Routes', function() {
-    var app, Cart, User, agent, Dream, aDream;
+describe("Cart Routes", function () {
+  var app, Cart, User, agent, Dream, aDream;
 
-    beforeEach('Sync DB', function() {
-        return db.sync({ force: true });
-    });
+  beforeEach("Sync DB", function () {
+    return db.sync({ force: true });
+  });
 
-    beforeEach('Create app', function() {
-        app = require('../../../server/app')(db);
-        Cart = db.model('orders');
-        User = db.model('user');
-        Dream = db.model('dream');
-        agent = supertest.agent(app);
-    });
+  beforeEach("Create app", function () {
+    app = require("../../../server/app")(db);
+    Cart = db.model("orders");
+    User = db.model("user");
+    Dream = db.model("dream");
+    agent = supertest.agent(app);
+  });
 
-    beforeEach('Create a dream', function(done) {
-        return Dream.create({
-            title: 'Hopes and Dreams',
-            description: 'Everything you want',
-            price: 12.33,
-            quantity: 1,
-            category: ["hi", "bye"],
-            photo: "I'm a photo :)"
-        }).then(function(dream) {
-            aDream = dream;
-            done();
-        }).catch(done);
-    });
-
-    beforeEach('Create a user', function(done) {
-
-
-        return User.create({
-            name: "Lorem Ipsum",
-            photo: "CoolPic.jpg",
-            phone: "1234567890",
-            email: "coolemail@abc.com",
-            password: "p@$$w0rD"
-        }).then(function(theUser) {
-            Cart.create({ status: 'Pending' })
-                .then(function(cart) {
-                    cart.setUser(theUser);
-                    cart.addDream(aDream);
-                    done();
-                })
-        }).catch(done);
+  beforeEach("Create a dream", function (done) {
+    return Dream.create({
+      title: "Hopes and Dreams",
+      description: "Everything you want",
+      price: 12.33,
+      quantity: 1,
+      category: ["hi", "bye"],
+      photo: "I'm a photo :)",
     })
+      .then(function (dream) {
+        aDream = dream;
+        done();
+      })
+      .catch(done);
+  });
 
-    describe("Request for a user\'s cart", function() {
-        it('should get a 200 response with the correct cart as the body', function(done) {
-            agent.get('/api/cart').expect(200).end(function(err, response) {
-                if (err) return done(err);
-                console.log("Am I a cart????", response.body)
-                expect(response.body).to.be.an('object');
-                expect(response.body.dreams).to.be.an('array');
-                done();
-            });
+  beforeEach("Create a user", function (done) {
+    return User.create({
+      name: "Lorem Ipsum",
+      photo: "CoolPic.jpg",
+      phone: "1234567890",
+      email: "coolemail@abc.com",
+      password: "p@$$w0rD",
+    })
+      .then(function (theUser) {
+        Cart.create({ status: "Pending" }).then(function (cart) {
+          cart.setUser(theUser);
+          cart.addDream(aDream);
+          done();
+        });
+      })
+      .catch(done);
+  });
+
+  describe("Request for a user's cart", function () {
+    it("should get a 200 response with the correct cart as the body", function (done) {
+      agent
+        .get("/api/cart")
+        .expect(200)
+        .end(function (err, response) {
+          if (err) return done(err);
+          console.log("Am I a cart????", response.body);
+          expect(response.body).to.be.an("object");
+          expect(response.body.dreams).to.be.an("array");
+          done();
         });
     });
+  });
 });
