0b0ceaec107b9e7d58440a6bffeb483fedd6b5ae
rushaliparikh <rushali@incubyte.co>
2024-02-01 02:08:37 +0000

    Migrate ReviewFactory to Angular

--- app-new/src/app/app.module.ts
+++ app-new/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ReviewService } from './common/ReviewFactory';
 import { OrderHistoryService } from './common/OrderHistoryFactory';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,7 +10,7 @@ import { UpgradeModule } from '@angular/upgrade/static';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, UpgradeModule],
-  providers: [OrderHistoryService, ],
+  providers: [ReviewService, OrderHistoryService, ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
