fa168ea799bee4056b61e21a396a228c68e7500d
Sapan Parikh <sapanparikh18@gmail.com>
2024-02-07 05:44:32 +0000

    Migrate ReviewFactory to Angular

--- app-new/src/app/app.module.ts
+++ app-new/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ReviewService } from './common/ReviewFactory';
 import { OrderHistoryService } from './common/OrderHistoryFactory';
 import { SecretStashService } from './common/SecretStash';
 import { NgModule } from '@angular/core';
@@ -10,7 +11,7 @@ import { UpgradeModule } from '@angular/upgrade/static';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, UpgradeModule],
-  providers: [OrderHistoryService, SecretStashService, ],
+  providers: [ReviewService, OrderHistoryService, SecretStashService, ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
